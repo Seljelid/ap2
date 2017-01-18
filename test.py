8f0652ca3920a94f79d001aca6fce87f3573add7
Seljelid <lars.eirikseljelid@gmail.com>
2017-01-18 11:07:43 +0100

    NaNs taken away, cleaner code

--- test.py
+++ test.py
@@ -1,26 +1,23 @@
 import numpy as np
 import csv
-import pandas
+import pandas as pd
 from datetime import datetime as dt
 
-with open('../Data/ai_dataset2.csv') as file:
-    df = pandas.read_csv(file,delimiter=',',low_memory='false')
+DATA_PATH = '../Data/ai_dataset2.csv'
 
-#Correlation matrix
-#corr = df.corr()
-#print corr
+def read_data(file):
+    with open(file) as file:
+        df = pd.read_csv(file,delimiter=',',low_memory='false')
+        n_nan = df.isnull().sum()   #count number of NaN
+        df.fillna(value=0,inplace='true')  #fill NaN with zero
+        data = df.as_matrix()
+        for row in data:
+            row[1] = dt.strptime(row[1],'%d%b%Y') #create datetime objects
 
-#Number of NaN
-print df.isnull().sum()
+        return (data,n_nan)
 
-#Averages
-#print 'Avg F1:', np.mean(df['F1'])
+(data,n_nan) = read_data(DATA_PATH)
+print(data[1:10,:])
+print(n_nan)
 
-#Convert to numpy array
-data = df.as_matrix()
 
-#Convert date
-for row in data:
-    row[1] = dt.strptime(row[1],'%d%b%Y')
-
-print data[1:10,:]

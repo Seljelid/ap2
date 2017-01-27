39906c7dec4ef04dd1f63b1d987e993d47103a03
Seljelid <lars.eirikseljelid@gmail.com>
2017-01-27 15:58:25 +0100

    Minor changes

--- test.py
+++ test.py
@@ -8,20 +8,18 @@ DATA_PATH = '../Data/ai_dataset2.csv'
 
 def read_data(file):
     with open(file) as file:
-        df = pd.read_csv(file,delimiter=',',low_memory='false')
+        df = pd.read_csv(file, delimiter=',', low_memory='false')
         n_nan = df.isnull().sum()   #count number of NaN
-        df.fillna(value=0,inplace='true')  #fill NaN with zero
+        #df.fillna(value=0,inplace='true')  #fill NaN with zero. Don't do this for now
         data = df.as_matrix()
         for row in data:
             row[1] = dt.strptime(row[1],'%d%b%Y') #create datetime objects
     return data, n_nan
 
 data, n_nan = read_data(DATA_PATH)
-print(data[1:10,:])
+print(data[-15000:-14990,:])
 print(n_nan/np.shape(data[0::,1]))
-
-
-
-
+unique, counts = np.unique(data[:,2], return_counts='True')
+print('Number of unique stocks', len(unique))
 
 
